d3328c85cb0bece974be98e97afda676a229f3cd
Letv1k <aleksejbelov278@gmail.com>
2024-12-18 21:54:18 +0300

    Загрузка

---
+++ code/data.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <array>
+#include "errors.h"
+
+using namespace std;
+
+class Data
+{
+public:
+    Data(
+        const string &path_to_conf,
+        const string &path_to_in,
+        const string &path_to_out);
+    array<string, 2> conf();
+    vector<vector<uint64_t>> read();
+
+    void write(const vector<uint64_t> &data);
+
+private:
+    string path_to_conf; ///< Путь к файлу конфигурации.
+    string path_to_in;   ///< Путь к входному файлу.
+    string path_to_out;  ///< Путь к выходному файлу.
+};
