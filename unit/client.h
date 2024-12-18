3df58b0790db8162abcd82452cda58bd4e0ee333
Letv1k <aleksejbelov278@gmail.com>
2024-12-18 22:24:33 +0300

    Загрузка

---
+++ unit/client.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+class Client
+{
+public:
+    Client(const string &address, uint16_t port);
+
+    void conn();
+    void auth(const string &username, const string &password);
+    vector<uint64_t> calc(const vector<vector<uint64_t>> &data);
+    void close();
+    
+private:
+    int socket;     ///< Сокет подключения.
+    string address; ///< Адрес сервера.
+    uint16_t port;  ///< Порт сервера.
+};
