583e8e7a1c092b70d642c71bc7a21d7028fe4f43
Letv1k <aleksejbelov278@gmail.com>
2024-12-18 23:00:45 +0300

    Загрузка

---
+++ doxygen/docs/html/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['read_0',['read',['../classData.html#a1f64d99ae5f90732eb7c6dbcce54a05e',1,'Data']]]
+];
---
+++ doxygen/docs/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['showhelp_0',['showHelp',['../classTerminal.html#a4fd718a80dbddd79c8cec2c15d4c8e3a',1,'Terminal']]]
+];
