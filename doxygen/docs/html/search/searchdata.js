2c5f3af2df49d8d0359afca1e4932c9d430a23fd
Letv1k <aleksejbelov278@gmail.com>
2024-12-18 22:52:24 +0300

    Загрузка

---
+++ doxygen/docs/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgimnprstw",
+  1: "abcdint",
+  2: "e",
+  3: "acdgprstw",
+  4: "fmn"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Функции",
+  4: "Переменные"
+};
+
